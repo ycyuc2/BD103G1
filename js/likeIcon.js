224cabaec7f22aefb81b2c5b7d00b386de19c8d3
Jo Chen <jo.chenlichiao@gmail.com>
2018-01-07 17:33:05 +0800

    新增喜翻不喜番按鍵js可以按按按

---
+++ js/likeIcon.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	var $likeCount    = 0;
+	var $dislikeCount = 0;
+
+	$('#like-num').html($likeCount);
+	$('#dislike-num').html($dislikeCount);
+
+	$('.like').click(function(){
+	  $likeCount++;
+	  $('#like-num').html($likeCount);
+	});
+
+	$('.dislike').click(function(){
+	  $dislikeCount--;
+	  $('#dislike-num').html($dislikeCount);
+	});
+});
