0d66ca57424866a0d6f786ec83f4cf08c107d455
cynthia <cindy141207@gmail.com>
2018-01-16 19:38:30 +0800

    ting

--- js/index.js
+++ js/index.js
@@ -2,12 +2,12 @@ $('document').ready(function(){
     $('.btn1').click(function(){
         $('#single').css('display', 'inline');
         $('#double').css('display', 'none');
-        $('html,body').delay(1000).animate({ scrollTop: $('.index_birth').offset().top }, 1000);
+        $('html,body').animate({ scrollTop: $('.index_birth').offset().top }, 1000);
     });
     $('.btn2').click(function () {
         $('#single').css('display', 'none');
         $('#double').css('display', 'inline');
-        $('html,body').delay(1000).animate({ scrollTop: $('.index_birth').offset().top }, 1000);
+        $('html,body').animate({ scrollTop: $('.index_birth').offset().top }, 1000);
     });
     $('.btnM.submit').click(function () {
         $('.index_result').css('display', 'inline');
