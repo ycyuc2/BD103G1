90c5c44a53567a5979cff9a185377954f14de8a0
pcytaco <35088655+pcytaco@users.noreply.github.com>
2018-01-20 01:40:01 +0800

    header require update

---
+++ php/publicHeader.php
@@ -0,0 +1,11 @@
+	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
+	<link rel="stylesheet" type="text/css" href="../css/dozen_nav.css">
+	<link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/footer.css">
+    <link rel="stylesheet" href="../css/btn.css">
+    <link rel="stylesheet" href="../css/showKarma.css">
+	<link rel="stylesheet" href="../css/loginLightbox.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+	<script src="../js/countDown.js"></script>
+	<script src="../js/loginLightbox.js"></script>
