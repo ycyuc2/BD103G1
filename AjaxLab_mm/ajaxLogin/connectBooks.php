7ce4eb4bbe9fb1e0e64576e69835bd72299936c4
pcytaco <35088655+pcytaco@users.noreply.github.com>
2018-01-22 21:00:25 +0800

    甲甲好雷

--- AjaxLab_mm/ajaxLogin/connectBooks.php
+++
@@ -1,7 +0,0 @@
-<?php
-  $dsn="mysql:host=localhost;dbname=books;charset=utf8;";
-  $user="web";
-  $password="780429";
-  $options=array( PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
-  $pdo = new PDO($dsn, $user, $password, $options);
-?>
--- AjaxLab_mm/ajaxLoginSendJsonToServer/ajaxIsLogin.php
+++
@@ -1,9 +0,0 @@
-<?php
-	ob_start();
-	session_start();
-	if ( isset($_SESSION['memName']) === true) {
-		echo $_SESSION['memName'];
-	}else{
-		echo "@@@no@@@";
-	}
-?>
