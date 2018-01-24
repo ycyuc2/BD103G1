9e8912dd274585571503c6f100cd11319643856c
pcytaco <35088655+pcytaco@users.noreply.github.com>
2018-01-25 06:54:24 +0800

    member trade

--- php/member.php
+++ php/member.php
@@ -1,3 +1,8 @@
+<?php 
+ob_start();
+session_start();
+ ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,7 +14,11 @@
 </head>
 <body>
 	
-<?php require_once("header.php") ?>
+<?php
+	require_once("connectBD103G1.php");
+	require_once("header.php"); 
+	$_SESSION["where"] = 'member.php';
+?>
 
 	
 	<div class="topBlank"></div>
@@ -133,27 +142,6 @@
 						echo "行號：",$ex->getLine(),"<br>";
 					}	
 					?>
-					<!-- <div class="tr">
-						<div class="td tdWidth replyNo">12</div>
-						<div class="td tdWidth replyContent">
-							<p>
-								[標題]標題標題標題
-							</p>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam sapiente voluptatibus iusto ipsam eaque, dicta culpa distinctio eum, ullam velit.
-							</p>
-						</div>
-						<div class="tdRight">
-							<div class="td tdWidth">
-								<p>老師名稱</p>
-								<p>1226 08:00</p>
-							</div>
-							<div class="td tdWidth">
-								<p>回文人名稱</p>
-								<p>1226 08:00</p>
-							</div>
-						</div>
-					</div> -->
 				</div>
 
 				
@@ -164,45 +152,70 @@
 			<div class="content trade">
 			
 				<h2 >交易紀錄</h2>
+				<?php $sql = "select * from order_list where mem_no = :mem_no";
+				$orderList = $pdo->prepare($sql);
+				$orderList->bindValue(':mem_no', $_SESSION["mem_no"]);
+				$orderList->execute();?>
 				<div class="table">
-					<div class="table">
 					<div class="tr">
-						<span class="medium">時間</span>
-						<span class="large">品項</span>
-						<span class="small">數量</span>
-						<span class="medium">金額</span>
-						<span class="close"></span>
+						<span >時間</span>
+						<span class="large">編號</span>
+						<span class="small"></span>
+						<span class="small">金額</span>
+						<span></span>
 					</div>
-					<div class="tr">
-							<span class="medium">2017/11/11</span>
-							<span class="large"></span>
-							<span class="small"></span>
-							<span class="medium"></span>
-							<span class="close">X</span>
-							
-							<div class="detail">
-								<span class="large">開運石獅子</span>
-								<span class="small">6</span>
-								<span class="medium">3333</span>
+					<?php while ($orderListRow = $orderList->fetchObject()) {?>
+						<div class="tr">
+							<span ><p><?php echo substr($orderListRow->order_time, 0, 10); ?></p><p><?php echo substr($orderListRow->order_time, 11, 8); ?></p></span>
+							<span class="large name <?php echo $orderListRow->order_no; ?>"><?php echo $orderListRow->order_no; ?></span>
+							<span class="small amount"></span>
+							<span class="small total"><?php echo $orderListRow->total; ?></span>
+							<span class="close"><i class="fa fa-angle-down" aria-hidden="true"></i></span>
+							<input type="checkbox" class="detailControl">
+							<div class = detailList>
+								<?php 
+								$sql = "select * from order_detail join products using(pd_no) where order_no = :order_no";
+								$orderDetail = $pdo->prepare($sql);
+								$orderDetail->bindValue(':order_no', $orderListRow->order_no);
+								$orderDetail->execute();
+								while ($orderDetailRow = $orderDetail->fetchObject()) {?>
+									<div class="detail">
+										<span class="large"><?php echo $orderDetailRow->pd_name; ?></span>
+										<span class="small"><?php echo $orderDetailRow->num ?></span>
+										<span class="small"><?php echo $orderDetailRow->num*$orderDetailRow->pd_price ?></span>
+									</div>
+								<?php } ?>
+								<div class="detail">
+									<span class="large"></span>
+									<span class="small">總計</span>
+									<span class="small"><?php echo $orderListRow->total; ?></span>
+								</div>
 							</div>
-							<div class="detail">
-								<span class="large">開運石獅子</span>
-								<span class="small">6</span>
-								<span class="medium">3333</span>
-							</div>
-							<div class="detail">
-								<span class="large">開運石獅子</span>
-								<span class="small">6</span>
-								<span class="medium">3333</span>
-							</div>
-							<div class="detail">
-								<span class="large"></span>
-								<span class="small">總計</span>
-								<span class="medium">9999</span>
-							</div>
-					</div>
+						</div>
+					<?php } ?>
 				</div>
-				</div>
+				<script type="text/javascript">
+					var close = document.querySelectorAll('.trade .table .tr .close');
+					for (var i = 0; i < close.length; i++) {
+						close[i].addEventListener('click', function () {
+							for (var i = 0; i < this.parentNode.childNodes.length; i++) {
+								console.log(this.parentNode.childNodes[i].tagName);
+							}
+							if(this.nextSibling.nextSibling.checked){
+								this.innerHTML = '<i class="fa fa-angle-down" aria-hidden="true"></i>';
+								this.parentNode.childNodes[3].innerText = 
+								this.parentNode.childNodes[3].className.split(' ')[2];
+								this.parentNode.childNodes[5].innerText = '';
+								this.parentNode.childNodes[11].checked = false;
+							}else{
+								this.innerHTML = '<i class="fa fa-angle-up" aria-hidden="true"></i>';
+								this.parentNode.childNodes[3].innerText = '項目';
+								this.parentNode.childNodes[5].innerText = '數量';
+								this.parentNode.childNodes[11].checked = true;
+							}
+						});
+					}
+				</script>
 			</div>
 		</div>
 		<div class="blank"></div>
