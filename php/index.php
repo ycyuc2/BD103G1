28860a8465a134fc767795a5ff4d19fc7409e1e9
pcytaco <35088655+pcytaco@users.noreply.github.com>
2018-01-28 11:09:35 +0800

    eeeee

--- php/index.php
+++ php/index.php
@@ -38,7 +38,7 @@ require_once 'connectBD103G1.php';
 
 	<!-- 前後端按鈕 -->
 	<div class="btn">
-		<span class="btnL"><a href="index.php" class="btnText btnText2">前端</a></span>
+		<span class="btnL"><a href="realIndex.php" class="btnText btnText2">前端</a></span>
 		<span class="btnL"><a href="bk_index.php" class="btnText btnText2 back">後端</a></span>
 	</div>
 
