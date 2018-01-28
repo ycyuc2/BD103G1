d8517fb9868d1a7e9677cf45776eb6f92f000dcc
ycyuc2 <kurenai1988@gmail.com>
2018-01-29 06:31:26 +0800

    asdf

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require_once 'php/connectBD103G1.php';
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="../css/loginLightbox.css">
+	<link rel="stylesheet" href="css/loginLightbox.css">
 	<link rel="stylesheet" type="text/css" href="css/guide.css">
 	<!-- <link rel="stylesheet" type="text/css" href="css/btn.css"> -->
 	<!-- <link rel="stylesheet" type="text/css" href="css/lightening.css"> -->
