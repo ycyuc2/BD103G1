2846ee8e7a293556a245f5f4329e25ecb9403a1c
ycyuc2 <kurenai1988@gmail.com>
2018-03-01 00:02:43 +0800

    預設大頭貼

--- php/register.php
+++ php/register.php
@@ -2,8 +2,8 @@
 	ob_start();
 	session_start();
 	require_once("connectBD103G1.php");
-	$sql = "insert into member(mem_acc, mem_psw, mem_nn, mem_tel, fort_sta, fort_no, obj_fort_no, karma_val, mem_sta)
-			value (:mem_acc, :mem_psw, :mem_nn, :mem_tel, :fort_sta, :fort_no, :obj_fort_no, :karma_val, :mem_sta)";
+	$sql = "insert into member(mem_acc, mem_psw, mem_nn, mem_tel, fort_sta, fort_no, obj_fort_no, karma_val, mem_sta, mem_pic)
+			value (:mem_acc, :mem_psw, :mem_nn, :mem_tel, :fort_sta, :fort_no, :obj_fort_no, :karma_val, :mem_sta, :mem_pic)";
 	$member = $pdo->prepare($sql);
 	$member -> bindValue(":mem_acc",$_POST["mem_acc"]);
 	$member -> bindValue(":mem_psw",$_POST["mem_psw"]);
@@ -14,6 +14,7 @@
 	$member -> bindValue(":obj_fort_no",$_SESSION["obj_fort_no"]);
 	$member -> bindValue(":karma_val",$_SESSION["karma_inc"]+100);
 	$member -> bindValue(":mem_sta",1);
+	$member -> bindValue(":mem_pic", "default.png");
 	$member -> execute();
 	$sql = "select * from member where mem_acc = :mem_acc and mem_psw = :mem_psw";
 	$member = $pdo->prepare($sql);
